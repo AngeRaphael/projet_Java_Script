6592d111eec5bff514baa8d539465aba8f547644
Ange Raphael LOUANNE <40097690+AngeRaphael@users.noreply.github.com>
2021-09-08 22:59:31 +0200

    Add files via upload

---
+++ devoir.js
@@ -0,0 +1,14 @@
+
+console.log("-------------------------------------");
+console.log("               EXERCICE 1            ");
+console.log("-------------------------------------");
+
+//Declaration des variables
+
+//Definition de la fonction : 
+
+//Test de la fonction :
+console.log(); 
+
+
+
